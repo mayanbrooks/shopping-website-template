7732c1d3a94dfb8b4489fdd191fdcb423f86d03d
Maya Noelle <maya.n.brooks@gmail.com>
2017-12-02 12:42:24 -0500

    commit 12.2

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,30 @@
-$('.carousel').carousel();
 
 
 
-$('.carousel.carousel-slider').carousel({fullWidth: true});
+// Goal: popup Loads as soon as the page loads. Make popup disappear upon user click
 
-// Next slide
+$(".close").on("click", function(){
+    $(".popup-overlay, .popup-content").removeClass("active-popup");
+});
 
-$('.carousel').click(function(){
-  $('.carousel').carousel('next'); // Move next n times.
+// Goal: Make hamburger menu nav appear and disappear based on user click
+
+$(".my_menu_btn").click(function(){
+   $(".my_responsive_menu").toggleClass("expand");
+   });
+
+
+
+// Goal: Change color of subscribe button to indicate user interactivity. Change text in button to let user know when "subscription" works.
+$(".subscribe_button_main").on("mouseover", function(){
+  $(this).css("background-color", "#f8bbd0");
+});
+
+
+$(".subscribe_button_main").on("mouseleave", function(){
+  $(this).css("background-color", "#fce4ec");
+});
+
+$(".subscribe_button_main").on("click", function(){
+  $(this).html("Subscribed!");
 });
